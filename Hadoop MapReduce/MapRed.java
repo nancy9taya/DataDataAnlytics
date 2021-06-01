2b1d6f669240690fba1c6c5d23195f3d1b93cafb
Nancy Hassan <61046681+nancy9taya@users.noreply.github.com>
2021-06-01 16:19:12 +0200

    Add files via upload

---
+++ Hadoop MapReduce/MapRed.java
@@ -0,0 +1,71 @@
+package mapRed;
+
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.Reducer.Context;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
+
+public class MapRed {
+	
+public static class TokenizerMapper
+      extends Mapper<LongWritable, Text, Text, IntWritable>{
+	  private Text word=new Text();
+       @Override 
+       public void map (LongWritable key, Text value,  Context context
+    		   ) throws IOException, InterruptedException {
+    	   StringTokenizer itr=new StringTokenizer(value.toString());
+    	   while(itr.hasMoreTokens()) {
+    		   word.set(itr.nextToken());
+    		   String [] tokens=word.toString().split(",");
+ 		       context.write(new Text(tokens[0]),new IntWritable(Integer.parseInt(tokens[1])));
+    	   }
+
+     }
+   }
+
+
+
+public static class kartReducer
+     extends Reducer<Text,IntWritable,Text,IntWritable> {
+  private IntWritable result = new IntWritable();
+  @Override 
+  public void reduce(Text key, Iterable<IntWritable> values,
+		  Context context
+                     ) throws IOException, InterruptedException {
+    int sum = 0;
+    for (IntWritable val : values) {
+      sum += val.get();
+    }
+    result.set(sum);
+    context.write(key, result);
+   
+  }
+}
+
+public static void main(String[] args) throws Exception {
+  Configuration conf = new Configuration();
+  Job job = Job.getInstance(conf, "outMapRed");
+  job.setJarByClass(MapRed.class);
+  job.setMapperClass(TokenizerMapper.class);
+  job.setCombinerClass(kartReducer.class);
+  job.setMapOutputKeyClass(Text.class);
+  job.setMapOutputValueClass( IntWritable.class);
+  job.setReducerClass(kartReducer.class);
+  job.setOutputKeyClass(Text.class);
+  job.setOutputValueClass(IntWritable.class);
+  FileInputFormat.addInputPath(job, new Path(args[0]));
+  FileOutputFormat.setOutputPath(job, new Path(args[1]));
+  System.exit(job.waitForCompletion(true) ? 0 : 1);
+}
+}
